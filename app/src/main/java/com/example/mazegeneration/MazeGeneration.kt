28959b05972afc9a77e4c8f78f45442e4e5e0743
busra <busra.selimoglu.97@hotmail.com>
2022-01-05 00:12:21 +0300

    Player Movement

--- app/src/main/java/com/example/mazegeneration/MazeGeneration.kt
+++ app/src/main/java/com/example/mazegeneration/MazeGeneration.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper.*
 import java.util.*
-import kotlin.collections.ArrayList
+import kotlin.math.abs
 import kotlin.properties.Delegates
 
 class MazeGeneration(context: Context, attrs: AttributeSet) : View(context, attrs) {
@@ -22,6 +25,15 @@ class MazeGeneration(context: Context, attrs: AttributeSet) : View(context, attr
 
     private var cells =Array(7){Array(10){Cell(7,10)} }
 
+    private var player = Cell(cols,rows)
+    private var exit = Cell(cols,rows)
+
+    private enum class Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
+
+
+
     init {
         createMaze()
     }
@@ -31,6 +43,14 @@ class MazeGeneration(context: Context, attrs: AttributeSet) : View(context, attr
         strokeWidth = 4F // wall thickness
         color = Color.BLACK
         Log.d("bk", "wallPaint")
+    }
+
+    private val playerPaint = Paint().apply {
+        color = Color.RED
+    }
+
+    private val exitPaint = Paint().apply {
+        color = Color.BLUE
     }
 
     private fun getNeighbour(cell: Cell) : Cell?{
@@ -95,7 +115,6 @@ class MazeGeneration(context: Context, attrs: AttributeSet) : View(context, attr
 
     private fun createMaze() {
 
-
         val stack = Stack<Cell>()
         var currentPosition: Cell
         var nextPosition: Cell?
@@ -105,6 +124,9 @@ class MazeGeneration(context: Context, attrs: AttributeSet) : View(context, attr
                 cells[x][y] = Cell(x,y)
             }
         }
+
+        player = cells[0][0]
+        exit = cells[cols-1][rows-1]
 
         // Starting point
         currentPosition = cells[0][0]
@@ -193,9 +215,89 @@ class MazeGeneration(context: Context, attrs: AttributeSet) : View(context, attr
                 }
             }
         }
+
+        // add the small margin wall
+        val margin : Float = cellSize.toFloat()/10
+
+        canvas.drawRect(
+            player.col*cellSize+margin,
+            player.row*cellSize+margin,
+            (player.col+1)*cellSize-margin,
+            (player.row+1)*cellSize-margin,
+            playerPaint
+        )
+        canvas.drawRect(
+            exit.col*cellSize+margin,
+            exit.row*cellSize+margin,
+            (exit.col+1)*cellSize-margin,
+            (exit.row+1)*cellSize-margin,
+            exitPaint
+        )
+
     }
 
+    private fun movePlayer(direction: Direction){
+        when(direction){
+            Direction.UP ->
+                if (!player.topWall){
+                player = cells[player.col][player.row-1]
+                }
+            Direction.DOWN ->
+                if (!player.bottomWall){
+                    player = cells[player.col][player.row+1]
+                }
+            Direction.LEFT ->
+                if (!player.leftWall){
+                    player = cells[player.col-1][player.row]
+                }
+            Direction.RIGHT ->
+                if (!player.rightWall){
+                    player = cells[player.col+1][player.row]
+                }
+        }
+        invalidate()
+    }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
 
+        if (event.action == MotionEvent.ACTION_DOWN) return true
 
+        if (event.action == MotionEvent.ACTION_MOVE){
+            var x : Float = event.x.toFloat()
+            var y : Float = event.y.toFloat()
+
+            var playerCenterX : Float = hMargin + (player.col.toFloat() + 0.5f)*cellSize
+            var playerCenterY : Float = vMargin + (player.row.toFloat() + 0.5f)*cellSize
+
+            //negative
+            var dx : Float = x - playerCenterX as Float
+            var dy : Float = y - playerCenterY as Float
+
+            // absolute
+            var absDx : Float = abs(dx)
+            var absDy : Float = abs(dy)
+
+            if (absDx > cellSize || absDy > cellSize){
+                // move in x-direction
+                if (absDx > absDy){
+                    if (dx > 0) { //move to the right
+                        movePlayer(Direction.RIGHT)
+                    }else{ // move to the left
+                        movePlayer(Direction.LEFT)
+                    }
+                }
+                // move in y-direction
+                else{
+                    if (dy > 0){ // move to the down
+                        movePlayer(Direction.DOWN)
+                    } else { // move to the up
+                        movePlayer(Direction.UP)
+                    }
+                }
+            }
+            return true
+        }
+
+        return super.onTouchEvent(event)
+    }
 }
